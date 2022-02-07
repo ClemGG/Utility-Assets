2b83eaeccaf01763dae4a31ae6eb4ee018614413
Clement Guichard <c.guichard508@laposte.net>
2022-02-07 19:53:55 +0100

    push init
    - Added EasyNotes & DevTex assets

---
+++ Assets/Utilities/Easy Notes/EasyNotes/Scripts/NoteComponent.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteComponent : MonoBehaviour
+{
+    public List<Note> notes = new List<Note>();
+
+    [System.Serializable]
+    public class Note 
+    {
+        public Color color = new Color(1, 1, 1, 1);
+        public string text, header;
+    }
+}
